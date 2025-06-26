ed82782490681a77ffba6cca42e816e9b3d90e2f
cabrera0laura <lucas56hoseok@gmail.com>
2025-06-26 01:39:54 -0300

    pdf 16

--- Controllers/RequerimentoController.cs
+++ Controllers/RequerimentoController.cs
@@ -2,17 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using LockAiMvc.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace LockAiMvc.Controllers
 {
     [Route("[controller]")]
     public class RequerimentoController : Controller
     {
-        public string uriBase = "xyz/Requerimento/"; //xyz --- enderco da minha API.
+        public string uriBase = "http://lauraCabrera20.somee.com/LockAi/requerimento/"; //xyz --- enderco da minha API.
 
         private readonly ILogger<RequerimentoController> _logger;
 
@@ -21,6 +23,82 @@ namespace LockAiMvc.Controllers
             _logger = logger;
         }
 
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> IndexAsync()
+        {
+            try
+            {
+                string uriComplementar = "GetAll"; //1
+                HttpClient httpClient = new HttpClient(); //2
+                string token = HttpContext.Session.GetString("SessionTokenUsuario"); //3
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token); //4
+
+                HttpResponseMessage response = await httpClient.GetAsync(uriBase + uriComplementar);
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    List<RequerimentoViewModel> listarequerimento = await Task.Run(() => JsonConvert.DeserializeObject<List<RequerimentoViewModel>>(serialized));
+                    return View(listarequerimento);
+                }
+                else
+                    throw new System.Exception(serialized);
+
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateAsync(RequerimentoViewModel r)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                /*string token = HttpContext.Session.GetString("SessionTokenUsuario");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);*/
+
+                var content = new StringContent(JsonConvert.SerializeObject(r));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = await httpClient.PostAsync(uriBase, content);
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    TempData["Mensagem"] = string.Format("Requerimento {0}, Id {1} salvo com sucesso!", r.Observacao, serialized);
+                    return RedirectToAction("Index");
+                }
+                else
+                    throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["Mensagem"] = ex.Message;
+                return RedirectToAction("Create");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+
+
+
+        /*
         public IActionResult Index()
         {
             return View();
@@ -32,7 +110,7 @@ namespace LockAiMvc.Controllers
             return View("Error!");
         }
 
-        [HttpGet]
+        [HttpGet("Index")]
         public ActionResult Index()
         {
             return View("CadastrarRequerimento");
@@ -51,6 +129,6 @@ namespace LockAiMvc.Controllers
                 return RedirectToAction("Index");
             }
             
-        }
+        }*/
     }
 }
